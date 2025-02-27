b8a117f5c38524a87498d9ce219a9fc4db66b9e7
taek105 <101572128+taek105@users.noreply.github.com>
2025-02-27 02:34:24 +0000

    test: local test

--- server/main.py
+++ server/main.py
@@ -6,6 +6,17 @@ from langchain_openai import ChatOpenAI
 from langchain.schema import HumanMessage
 from chromadb import PersistentClient
 
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 from dotenv import load_dotenv
 
 load_dotenv()
